1916e252e8d37b0d0a0d084f89e7ebb8a1bf29a6
frectonz <fraol0912@gmail.com>
2023-09-30 11:24:12 +0300

    feat: create initial nom parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,82 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use nom::{
+    bytes::complete::{tag, take_while},
+    character::is_alphanumeric,
+    combinator::map,
+    multi::separated_list0,
+    IResult,
+};
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Surf<'a> {
+    host: &'a str,
+    path: Vec<&'a str>,
+    query: HashMap<&'a str, &'a str>,
+    fragment: Option<&'a str>,
+}
+
+fn is_letter(c: char) -> bool {
+    is_alphanumeric(c as u8)
+}
+
+fn is_letter_or_dot(c: char) -> bool {
+    is_alphanumeric(c as u8) || c == '.'
+}
+
+pub fn parse_surf<'a>(input: &'a str) -> IResult<&'a str, Surf<'a>> {
+    let (input, _) = tag("grid!")(input)?;
+    let (input, host) = take_while(is_letter_or_dot)(input)?;
+    let (input, path) = map(
+        separated_list0(tag("/"), take_while(is_letter)),
+        |mut list| {
+            list.remove(0);
+            list
+        },
+    )(input)?;
+
+    Ok((
+        input,
+        Surf {
+            host,
+            path,
+            query: HashMap::default(),
+            fragment: None,
+        },
+    ))
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{parse_surf, Surf};
+    use std::collections::HashMap;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn simple_surf() {
+        let (_, surf) = parse_surf("grid!example.com").unwrap();
+
+        assert_eq!(
+            surf,
+            Surf {
+                host: "example.com",
+                path: Vec::default(),
+                query: HashMap::default(),
+                fragment: None,
+            }
+        );
+    }
+
+    #[test]
+    fn parse_path() {
+        let (_, surf) = parse_surf("grid!example.com/with/a/path").unwrap();
+
+        assert_eq!(
+            surf,
+            Surf {
+                host: "example.com",
+                path: ["with", "a", "path"].into(),
+                query: HashMap::default(),
+                fragment: None,
+            }
+        );
     }
 }
